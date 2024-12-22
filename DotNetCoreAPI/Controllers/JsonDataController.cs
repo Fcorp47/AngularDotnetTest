72d1d0447d38ce4e64aa4342e1543f1c94e6e06a
Arjun <arjunmpoduval@gmail.com>
2024-12-22 09:05:59 +0530

    Completed task wep api

---
+++ DotNetCoreAPI/Controllers/JsonDataController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreAPI.Controllers
+{
+    [ApiController]
+    public class JsonDataController : ControllerBase
+    {
+        //test
+    }
+}
